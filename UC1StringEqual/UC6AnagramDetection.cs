32a7738ae6e70cfb0166fe8ac6e0583d8f4b5c37
Amit <amitkr24031995@gmail.com>
2021-10-08 18:35:43 +0530

    Added UC6AnagramDetection

---
+++ UC1StringEqual/UC6AnagramDetection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UC6AnagramDetection
+{
+    class UC7Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "abcd";
+            string str2 = "bcda";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string va1 = new string(ch1);
+            string va2 = new string(ch2);
+
+            if (va1 == va2)
+            {
+                Console.WriteLine("String is Equal");
+            }
+            else
+            {
+                Console.WriteLine("String is not Equal");
+            }
+        }
+    }
+}
