253626a2eae0074f5d22a3d92e652000f6dcb1e6
Amit <amitkr24031995@gmail.com>
2021-10-08 18:29:25 +0530

    Adding UC7PrimeNumber

---
+++ UC1StringEqual/UC7PrimeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumberExm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsPrime = true;
+            int i, j;
+            Console.WriteLine("Prime Number is : ");
+            for (i = 2; i <= 100; i++)
+            {
+                for (j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                }
+                if (IsPrime)
+                {
+                    Console.WriteLine("\n" + i);
+                }
+                IsPrime = true;
+            }
+        }
+    }
+}
